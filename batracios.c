a45bceafe5bea7d1572be8f24943cbb1a44813c9
n0nuser <pjgr2000@gmail.com>
2020-03-26 20:25:37 +0100

    Upload!

--- batracios.c
+++ batracios.c
@@ -67,6 +67,7 @@ int main (int argc, char*argv[]){
 
 	if (param2 <= 0) {
 		write(2, "Ha introducido un tiempo de partos incorrecto.\nIntroduzca un tiempo mayor de 0", strlen("Ha introducido un tiempo de partos incorrecto.\nIntroduzca un tiempo mayor de 0"));
+		exit(2);
 	}
 
 	/* ============= Creacion Recursos ============= */
@@ -80,69 +81,88 @@ int main (int argc, char*argv[]){
 		exit(1);
 	}
 
-	//Memoria compartida
-	if((mem=shmget(IPC_PRIVATE,sizeof(int)*500,IPC_CREAT|0600)) == -1) {
-		perror("Error: No se pudo crear la mem compartida.");
-		exit(2);
+	/* ============= Creación de la mem compartida ============= */
+
+	mem = -1;
+	ptr = (char*) -1;
+
+	// Memoria compartida
+	if((mem = shmget(IPC_PRIVATE,sizeof(int)*500,IPC_CREAT|0600))==-1) {
+		perror("Error al crear la mem compartida.\n");
+		exit(3);
 	}
 
-	if((ptr = (char*) shmat(mem,NULL,0)) == NULL) {
-		perror("Error: No se pudo asignar el puntero de sentido de mem compartida");
-		exit(2);
+	// Puntero a la ptr de memoria compartida
+	if((ptr = (char *) shmat(mem,NULL,0)) == NULL) {
+		perror("Error al crear puntero a ptr compartida.\n");
+		exit(3);
 	}
 
-	for(j=0; j<25; j++)
-	{
-		movX=(int *)(ptr+2048+j*8);
-		movY=(int*)(ptr+2048+j*8+4);
-		*movX=-1;
-		*movY=-1;
+	/* ============= Inicialización de los punteros mov ============= */
+
+	for (j=0; j<25; j++) {
+		movX = (int*)(ptr+2048+j*8);
+		movY = (int*)(ptr+2048+j*8+4);
+		*movX = -1;
+		*movY = -1;
 	}
 
+
+	/* ============= Inicialización de los contadores ============= */
+	//Memoria
 	r_salvadas = (int*)(ptr+2048+51*sizeof(int));
-	r_nacidas = (int*)(ptr+2048+52*sizeof(int));
-	r_perdidas = (int*)(ptr+2048+53*sizeof(int));
+	//Valor
 	*r_salvadas = 0;
+
+	r_nacidas = (int*)(ptr+2048+52*sizeof(int));
 	*r_nacidas = 0;
+
+	r_perdidas = (int*)(ptr+2048+53*sizeof(int));
 	*r_perdidas = 0;
 
+	/* ============= Creación del lote de semáforos ============= */
 	sem=semget(IPC_PRIVATE,14,IPC_CREAT|0600);
 	if(sem==-1) {
 		perror("error en la creacion de los sems");
 		exit(2);
 	}
 
-	//Semáforo para controlar el número de procesos
+
+/* ============= Inicialización del lote de semáforos ============= */
+
+	// Incluir un semaforo 0 que es nuestro semaforo de procesos maximos
+
+	//PROCESOS MAXIMOS //Semáforo 1 (Se encarga del parto de la 1º rana madre)
 	if(semctl(sem,1,SETVAL, sem1)==-1) {
 		perror("error al inicializar el sem 1");
 		exit(2);
 	}
 
-	//Semáforo para controlar el nacimiento de la rana 1
+	//RANA1 // Semáforo 2 (Se encarga del parto de la 2º rana madre)
 	if(semctl(sem,2,SETVAL,sem2)==-1) {
 		perror("error al inicializar el sem 2");
 		exit(2);
 	}
 
-	//Semáforo para controlar el nacimiento de la rana 2
+	//RANA2 // Semáforo 3 (Se encarga del parto de la 3º rana madre)
 	if(semctl(sem,3,SETVAL,sem3)==-1) {
 		perror("error al inicializar el sem 3");
 		exit(2);
 	}
 
-	//Semáforo para controlar el nacimiento de la rana 3
+	//RANA3 // Semáforo 4 (Se encarga del parto de la 4º rana madre)
 	if(semctl(sem,4,SETVAL,sem4)==-1) {
 		perror("error al inicializar el sem 4");
 		exit(2);
 	}
 
-	//Semáforo para controlar el nacimiento de la rana 4
+	//RANA4 // Semáforo 5 (Se encarga de la memoria compartida)
 	if(semctl(sem,5,SETVAL,sem5)==-1) {
 		perror("error al inicializar el sem 5");
 		exit(2);
 	}
 
-	//Semáforo para controlar la mem compartida
+	//MEM COMPARTIDA // Semáforo 10 NO existe
 	if(semctl(sem,10,SETVAL,sem10)==-1) {
 		perror("error al inicializar el sem 10");
 		exit(2);
@@ -155,7 +175,7 @@ int main (int argc, char*argv[]){
 		exit(1);
 	}
 
-	/* ============= Creacion Ranas ============= */
+	/* ============= Creación de las ranas y ranitas ============= */
 
 	for(i=0; i<4; i++) {
 
@@ -191,7 +211,7 @@ int main (int argc, char*argv[]){
 
 	while(!noTerminado) {
 		for(z = 0; z < 7; z++) {
-			sems.sem_num = 10;    //sem de control de mem compartida
+			sems.sem_num = 10;
 			sems.sem_op = -1;
 			sems.sem_flg = 0;
 			if(semop(sem,&sems,1) == -1) {
@@ -199,9 +219,7 @@ int main (int argc, char*argv[]){
 				perror("Error sem de choque: ");
 			}
 
-			if(BATR_avance_troncos(z)==-1) {
-				perror("Error: No pudieron avanzar los troncos de la fila 0");
-			}
+			if(BATR_avance_troncos(z)==-1) perror("Error: No pudieron avanzar los troncos de la fila 0");
 
 			for(j=0; j<25; j++)
 			{
@@ -209,28 +227,27 @@ int main (int argc, char*argv[]){
 				movY = (int*)(ptr+2048+j*8+4);
 
 				if((*movY) == 10-z) {
-					if(dirs[z]==0) (*movX)++;
-					else (*movX)--;
+					if(dirs[z]==0)
+						(*movX)++;
+					else
+						(*movX)--;
 				}
 			}
-
-			sems.sem_num = 10;  //sem de control de mem compartida
+			sems.sem_num = 10;
 			sems.sem_op = 1;
 			sems.sem_flg = 0;
-			if(semop(sem,&sems,1) == -1) {
-				perror("Error sem de choque: ");
-			}
+			if(semop(sem,&sems,1) == -1) perror("Error sem de choque: ");
 
-			if(BATR_pausita()==-1) {
-				perror("Error: No se pudo realizar \"pausita\" de la fila 0");
-			}
+			if(BATR_pausita()==-1) perror("Error: No se pudo realizar \"pausita\" de la fila 0");
 		}
 	}   //Fin bucle infinito (troncos)
 	finPrograma();
 }
 
 
-// FUNCIONES
+/* ============= FUNCIONES ============= */
+
+/* ============= Rana ============= */
 
 void rana(int i){
 	int j;
@@ -238,7 +255,7 @@ void rana(int i){
 	while(!noTerminado) {
 		BATR_descansar_criar();
 
-		sems.sem_num = i+2;     //sem de control de nacimiento
+		sems.sem_num = i+2;
 		sems.sem_op = -1;
 		sems.sem_flg = 0;
 		if(semop(sem,&sems,1)==-1) {
@@ -246,7 +263,7 @@ void rana(int i){
 			perror("Error sem de control de nacimiento: ");
 		}
 
-		sems.sem_num = 1;      //sem de control de procesos maximos
+		sems.sem_num = 1;
 		sems.sem_op = -1;
 		sems.sem_flg = 0;
 		if(semop(sem,&sems,1)==-1) {
@@ -254,13 +271,12 @@ void rana(int i){
 			perror("Error sem de control de procesos: ");
 		}
 
-		//sem de control de mem compartida (506)
 		sems.sem_num = 10;
 		sems.sem_op = -1;
 		sems.sem_flg = 0;
 		if(semop(sem,&sems,1) == -1) {
 			if(errno==EINTR) {
-				sems.sem_num = 1;      //sem de control de procesos maximos
+				sems.sem_num = 1;
 				sems.sem_op = 1;
 				sems.sem_flg = 0;
 				if(semop(sem,&sems,1)==-1) perror("Error sem de control de procesos: ");
@@ -270,7 +286,6 @@ void rana(int i){
 
 		//Por qué no un 20? DEBUG
 		for(j=0; j<25; j++) {
-			//Se mueve acordes a la rana madre
 			//2048?? DEBUG
 			movX=(int *)(ptr+2048+j*8);
 			movY=(int *)(ptr+2048+j*8+4);
@@ -305,29 +320,28 @@ void rana(int i){
 					}
 					ranita(j, i);
 					break;
-				}  //FIN DEL SWITCH
-				ptr= (char*) shmat(mem,NULL,0);
+				}
+				ptr = (char*)shmat(mem,NULL,0);
 				break;
 
 			}
 
 			if(j==24) {
-				sems.sem_num = 1;      //sem de control de procesos maximos
+				sems.sem_num = 1;
 				sems.sem_op = 1;
 				sems.sem_flg = 0;
 				if(semop(sem,&sems,1)==-1) perror("Error sem de control de procesos (9): ");
 			}
 		}
 
-		//Sem de control de mem compartida (269)
 		sems.sem_num = 10;
 		sems.sem_op = 1;
 		sems.sem_flg = 0;
-		if(semop(sem,&sems,1) == -1) {
-			perror("Error sem de mem compartida (10): ");
-		}
+		if(semop(sem,&sems,1) == -1) perror("Error sem de mem compartida (10): ");
 	}
 }
+
+/* ============= Ranita ============= */
 
 void ranita(int i, int madre) {
 
@@ -340,7 +354,7 @@ void ranita(int i, int madre) {
 
 	while(!noTerminado)
 	{
-		sems.sem_num = 10;      //sem de control de mem compartida
+		sems.sem_num = 10;
 		sems.sem_op = -1;
 		sems.sem_flg = 0;
 		if(semop(sem,&sems,1) == -1) {
@@ -349,25 +363,19 @@ void ranita(int i, int madre) {
 			perror("Error sem de mem compartida: ");
 		}
 
-
 		movX = (int*)(ptr+2048+i*8);
 		movY = (int*)(ptr+2048+i*8+4);
 
-
 		if((*movX) < 0 || (*movX) > 79)
 		{
-			sems.sem_num = 10;        //sem de control de mem compartida
+			sems.sem_num = 10;
 			sems.sem_op = 1;
 			sems.sem_flg = 0;
-			if(semop(sem,&sems,1) == -1) {
-				perror("Error sem de mem compartida: ");
-			}
+			if(semop(sem,&sems,1) == -1) perror("Error sem de mem compartida: ");
 
 			(*r_perdidas)++;
-
 			(*movY) = -1;
 			(*movX) = -1;
-
 			break;
 		}
 
@@ -375,13 +383,10 @@ void ranita(int i, int madre) {
 		else if(BATR_puedo_saltar((int)(*movX),(int)(*movY),IZQUIERDA)==0) sentido = IZQUIERDA;
 		else if(BATR_puedo_saltar((int)(*movX),(int)(*movY),DERECHA)==0) sentido = DERECHA;
 		else{
-
-			sems.sem_num = 10;        //sem de control de mem compartida
+			sems.sem_num = 10;
 			sems.sem_op = 1;
 			sems.sem_flg = 0;
-			if(semop(sem,&sems,1) == -1) {
-				perror("Error sem de mem compartida: ");
-			}
+			if(semop(sem,&sems,1) == -1) perror("Error sem de mem compartida: ");
 
 			BATR_pausa();
 			continue;
@@ -397,38 +402,32 @@ void ranita(int i, int madre) {
 			exit(1);
 		}
 
-		sems.sem_num = 10;          //sem de control de mem compartida
+		sems.sem_num = 10;
 		sems.sem_op = 1;
 		sems.sem_flg = 0;
-		if(semop(sem,&sems,1) == -1) {
-			perror("Error sem de mem compartida: ");
-		}
+		if(semop(sem,&sems,1) == -1) perror("Error sem de mem compartida: ");
 
 		BATR_pausa();
 
-		sems.sem_num = 10;  //sem de control de mem compartida
+		sems.sem_num = 10;
 		sems.sem_op = -1;
 		sems.sem_flg = 0;
 		if(semop(sem,&sems,1) == -1) {
 			if(errno==EINTR) break;
-
 			perror("Error sem de mem compartida: ");
 		}
 
 		movX = (int*)(ptr+2048+i*8);
 		movY = (int*)(ptr+2048+i*8+4);
 
-
 		if((*movX) < 0 || (*movX) > 79)
 		{
-			sems.sem_num = 10;        //sem de control de mem compartida
+			sems.sem_num = 10;
 			sems.sem_op = 1;
 			sems.sem_flg = 0;
-			if(semop(sem,&sems,1) == -1) {
-				perror("Error sem de mem compartida: ");
-			}
-			(*r_perdidas)++;
+			if(semop(sem,&sems,1) == -1) perror("Error sem de mem compartida: ");
 
+			(*r_perdidas)++;
 			(*movY) = -1;
 			(*movX) = -1;
 			break;
@@ -442,50 +441,39 @@ void ranita(int i, int madre) {
 
 		if((*movY)==11) {
 
-			sems.sem_num = 10;        //sem de control de mem compartida
+			sems.sem_num = 10;
 			sems.sem_op = 1;
 			sems.sem_flg = 0;
-			if(semop(sem,&sems,1) == -1) {
-				perror("Error sem de mem compartida: ");
-			}
-			(*r_salvadas)++;
+			if(semop(sem,&sems,1) == -1) perror("Error sem de mem compartida: ");
 
+			(*r_salvadas)++;
 			(*movY) = -1;
 			(*movX) = -1;
-
-
 			break;
 		}
 
 		if((*movY) == 1 && nacimiento == 0)
 		{
 			nacimiento = 1;
-			sems.sem_num = madre+2;       //sem de control de nacimiento
+			sems.sem_num = madre+2;
 			sems.sem_op = 1;
 			sems.sem_flg = 0;
-			if(semop(sem,&sems,1)==-1) {
-				perror("Error sem de control de nacimiento");
-			}
+			if(semop(sem,&sems,1)==-1) perror("Error sem de control de nacimiento");
 		}
 
-		sems.sem_num = 10;      //sem de control de mem compartida
+		sems.sem_num = 10;
 		sems.sem_op = 1;
 		sems.sem_flg = 0;
-		if(semop(sem,&sems,1) == -1) {
-			perror("Error sem de mem compartida: ");
-		}
-
-	}   // Fin bucle infinito
-
-	sems.sem_num = 1;    //sem de control de procesos maximos
-	sems.sem_op=1;
-	sems.sem_flg=0;
-	if(semop(sem,&sems,1)==-1) {
-		perror("Error sem de control de procesos: ");
+		if(semop(sem,&sems,1) == -1) perror("Error sem de mem compartida: ");
 	}
 
+	sems.sem_num = 1;
+	sems.sem_op=1;
+	sems.sem_flg=0;
+	if(semop(sem,&sems,1)==-1) perror("Error sem de control de procesos: ");
+
 	exit(0);
-}    //Fin funcion */
+}
 
 
 void intHandler(int a) {
@@ -496,48 +484,35 @@ void finPrograma()
 {
 	int n;
 
-
 	if (BATR_fin() == -1) {
 		fprintf(stderr, "Error: No se pudo finalizar\n");
 		fflush(stderr);
 		exit(1);
 	}
 
-	for(n=0; n<4; n++)
-		wait(NULL);
+	for(n=0; n<4; n++) wait(NULL);
 
-
-	sems.sem_num = 1;    //sem de control de mem compartida
+	sems.sem_num = 1;
 	sems.sem_op = -25;
 	sems.sem_flg = 0;
-	if(semop(sem,&sems,1) == -1)
-		perror("Error sem de mem compartida (777): ");
+	if(semop(sem,&sems,1) == -1) perror("Error sem de mem compartida (777): ");
 
-
-	sems.sem_num = 0;    //sem de control de mem compartida
+	sems.sem_num = 0;
 	sems.sem_op = 1;
 	sems.sem_flg = 0;
-	if(semop(sem,&sems,1) == -1) {
-		perror("Error sem de mem compartida (888): ");
-	}
+	if(semop(sem,&sems,1) == -1) perror("Error sem de mem compartida (888): ");
 
-	if(BATR_comprobar_estadIsticas(*r_nacidas, *r_salvadas, *r_perdidas)==-1) {
-		perror("Error: No ha funcionado \"estadisticas\"\n");
-	}
+	if(BATR_comprobar_estadIsticas(*r_nacidas, *r_salvadas, *r_perdidas)==-1) perror("Error: No ha funcionado \"estadisticas\"\n");
 
-	if (semctl(sem, 0, IPC_RMID) == -1) {
+	if (semctl(sem, 0, IPC_RMID) == -1)
 		perror("Error: NO se pudo destruir el sem.\n");
-
-	}else{
+	else
 		printf("Semaforo destruido.\n");
-	}
 
 	if (shmctl(mem, IPC_RMID, 0) == -1) {
 		perror("Error: NO se pudo destruir la mem compartida");
 		exit(1);
-	}else{
-		printf("Memoria compartida destruida\n");
-	}
+	} else printf("Memoria compartida destruida\n");
 
 	exit(1);
 }
